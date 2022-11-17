c8569fed4c1204ad4d62548ba2619d10d897fada
TIWARISUSHMITA <sushmitatiwari.rewa@gmail.com>
2022-11-17 11:25:22 +0530

    oki

---
+++ java/exceptionHandling/Two.java
@@ -0,0 +1,23 @@
+class  Two
+{
+    public static void main(String[] args)
+    {
+        try{
+            System.out.println(10/0);
+        }catch(Exception e)
+        {
+e.printStackTrace();
+        
+        System.out.println(e.toString());
+        //System.out.println(ex);
+        System.out.println(e.getMessage());
+        }
+    }
+}
+
+/*
+java.lang.ArithmeticException: / by zero
+        at Two.main(Two.java:6)
+java.lang.ArithmeticException: / by zero
+/ by zero
+*/
